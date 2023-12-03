ca55840fad7a1962c4a7d980d0fca4856f4b46fd
krejenel <148277802+krejenel@users.noreply.github.com>
2023-12-03 12:18:52 +0200

    Sortare_prin_selectie.cpp

---
+++ Algoritmi/Sortare_prin_selectie.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+ifstream f("date.in");
+
+int main()
+{
+    int v[100],n,i,minn,j,p;
+    f>>n;
+    for(i=1; i<=n; i++)
+        f>>v[i];
+
+    for(i=1; i<=n-1; i++)
+    {
+        minn=v[i];
+        p=i;
+        for(j=i+1; j<=n; j++)
+            if(v[j]<minn)
+            {
+                minn=v[j];
+                p=j;
+            }
+        swap(v[i],v[p]);
+    }
+
+    for(i=1; i<=n; i++)
+        cout<<v[i]<<" ";
+
+    return 0;
+}

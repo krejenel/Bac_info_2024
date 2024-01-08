2bfb3336b1be98eb4cadd27482688e7f3a72c24b
Nimo Killorbernt <148277802+krejenel@users.noreply.github.com>
2024-01-08 21:37:02 +0200

    Problema_8.cpp

---
+++ Capitolul_3/Problema_8.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main()
+{
+    int n=5678;
+    while(n!=0)
+    {
+        cout<<n<<endl;
+        n=n/10;
+    }
+    return 0;
+}

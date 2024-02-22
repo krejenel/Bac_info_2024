15079bf2bca019d5fb0342bfa56565161e4031a4
Nimo Killorbernt <148277802+krejenel@users.noreply.github.com>
2024-02-22 07:39:36 +0200

    Problema_1.cpp

---
+++ bac.info.oficial.2009/Varianta_9/Problema_1.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+int main()
+{   
+  
+    int n,k;
+    cin>>n>>k;
+    while(n!=0)
+    {
+      cout<<k*n<<" ";
+        n--;
+    }
+ 
+    return 0;
+}

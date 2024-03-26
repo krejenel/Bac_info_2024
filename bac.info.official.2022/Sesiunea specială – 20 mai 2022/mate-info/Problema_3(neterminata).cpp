fceb648fc82e349d4a1c10b82d3ad609794cf232
Nimo Killorbernt <148277802+krejenel@users.noreply.github.com>
2024-03-26 10:35:17 +0200

    Update Problema_3.cpp

--- bac.info.official.2022/Sesiunea specială – 20 mai 2022/mate-info/Problema_3(neterminata).cpp
+++ bac.info.official.2022/Sesiunea specială – 20 mai 2022/mate-info/Problema_3(neterminata).cpp
@@ -1,40 +1,44 @@
 #include <iostream>
 #include <bits/stdc++.h>
+#include <values.h>
 using namespace std;
 
 ifstream f("date.in");
 
 int main()
 {
-    int maxx=0, x, nr=1, aux=0,ok=1, auxx=0;
+    int aux=0,x,maxx=0,nr=1,lung=0;
     f>>x;
     aux=x;
     while(f>>x)
     {
-        if(nr>x)
+        if(x!=aux)
         {
-            if(maxx<auxx)
-                maxx=auxx;
-        }
-        else if(x!=aux && nr==aux)
-        {
-            auxx=auxx+nr;
+            if(nr==aux)
+            {
+                lung=lung+nr;
+            }
+            else
+            {
+                if(lung>maxx)
+                    maxx=lung;
+                lung=0;
+            }
             nr=1;
-        }
-        else if(x=aux && nr!=aux)
-        {
-            if(maxx<auxx)
-                maxx=auxx;
-            auxx=0;
+
         }
         else if(x==aux)
             nr++;
 
-        x=aux;
-
+        aux=x;
     }
+    if(nr==aux)
+    {
+        lung=lung+nr;
+    }
+    if(lung>maxx)
+        maxx=lung;
     cout<<maxx;
-
 
     return 0;
 }
